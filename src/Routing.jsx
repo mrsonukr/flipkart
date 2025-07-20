70a476335bcd4057b25eb13b74d9279900dfeb4d
mrsonukr <MSSONUKR@GMAIL.COM>
2025-07-20 22:47:48 +0530

    Update Routing.jsx

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -32,7 +32,7 @@ const Routing = () => {
     <Suspense
       fallback={
         <div className="fixed inset-0 bg-white flex justify-center items-center z-50">
-          <div className="w-12 h-12 border-[5px] border-blue-500 border-dashed rounded-full animate-spin"></div>
+          <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent border-solid rounded-full animate-spin"></div>
         </div>
       }
     >
